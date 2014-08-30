21a8490868454c4bfd03e6cd7e9dce3a04d6403b
Ian Mc Shane <icmcshane@gmail.com>
2014-08-30 10:44:58 +0200

    wip - added clojure

--- scripts/tools/clojure.sh
+++ scripts/tools/clojure.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Create directory
+if [ ! -d ~/bin ]; then
+  mkdir ~/bin
+fi
+
 # Leiningen
 if [ ! -r ~/bin/lein ]; then
     wget -O ~/bin/lein https://github.com/technomancy/leiningen/raw/stable/bin/lein
